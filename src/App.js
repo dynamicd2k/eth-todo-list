7b1e6dd817541a7d68394f6800eb154ffcb4bddd
Rogue Guy <rogueguy@Rogues-MacBook-Air.local>
2021-04-24 16:42:56 +0530

    Web3 services added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React ,{ Component } from 'react';
+import Web3 from 'web3';
 
-function App() {
+class App extends Component {
+  componentWillMount(){
+    this.loadBlockchainData();
+  }
+  async loadBlockchainData(){
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+    const network = await web3.eth.net.getNetworkType();
+    const accounts = await web3.eth.getAccounts();
+    const balance = await web3.eth.getBalance(accounts[0]);
+    this.setState({account:accounts[0], network:network, balance: balance});
+  }
+
+  constructor(props){
+    super(props);
+    this.state={account:'', network:'', balance: ''};
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello World.</h1>
+      <p>Your account: {this.state.account}</p>
+      <p>Your network: {this.state.network}</p>
+      <p>Your balance: {this.state.balance}</p>
     </div>
   );
+  }
 }
 
 export default App;
